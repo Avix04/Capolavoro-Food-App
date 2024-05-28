ae625c7340fef783bf0847d2005fcf1af61e798f
Avix04 <andrea.avisani@gmail.com>
2024-05-28 15:49:02 +0200

    First Commit

---
+++ lib/providers/meals_provider.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_application_1/data/dummy_data.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final mealsProvider = Provider((ref) {
+  return dummyMeals;
+});
